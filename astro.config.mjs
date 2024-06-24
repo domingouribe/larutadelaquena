9408564b7a27115c5770d456369910dbd517e902
domingouribe <uribe.domingo@gmail.com>
2024-06-24 14:15:02 -0400

    Canonical URL fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import partytown from "@astrojs/partytown";
 // https://astro.build/config
 export default defineConfig({
 
+  site: 'https://larutadelaquena.com',
   prefetch: {
     prefetchAll: true
   },
