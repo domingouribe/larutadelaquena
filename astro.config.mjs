d2b0be82390dc646243e2fe9a4ff3cbc495429b4
domingouribe <uribe.domingo@gmail.com>
2024-06-27 15:58:01 -0400

    Partytown deleted

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
-import partytown from '@astrojs/partytown';
 
 export default defineConfig({
   site: 'https://larutadelaquena.com',
   prefetch: {
     prefetchAll: true
-  },
-  integrations: [partytown()],
+  }
 });
